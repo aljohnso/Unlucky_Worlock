66365853c35bc1167135ae35f0f68ae28d07e28c
Alasdair Johnson <aljohnso@students.pitzer.edu>
2016-11-21 17:46:47 -0800

    Creating DB connection for website
    PyCharm POA project

--- PyCharmPOAProject/DataBase/DataBaseControls/FlaskDatabaseMangment.py
+++ PyCharmPOAProject/DataBase/DataBaseControls/FlaskDatabaseMangment.py
@@ -4,41 +4,41 @@ from flask import Flask, g
 
 app = Flask(__name__)
 
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-def init_db():
-    """
-    will go to the schema flile
-    :return: VOID
-    """
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-def show_entries(SQL):
-    """
-    Example show_entries('select title, text from entries order by id desc')
-    :param SQL: SQL code that will call table info to be returned
-    :return: entries form sql table look up
-    """
-    db = get_db()
-    cur = db.execute(SQL)
-    entries = cur.fetchall()
-    return entries
+# wont work becuese DATABASE is configed in our app Im sure there is a work around but I am under time pressuere rn
+# def connect_db():
+#     """Connects to the specific database."""
+#     rv = sqlite3.connect(app.config['DATABASE'])
+#     rv.row_factory = sqlite3.Row
+#     return rv
+#
+# # Calls connect DB
+# def get_db():
+#     """Opens a new database connection if there is none yet for the
+#     current application context.
+#     """
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = connect_db()
+#     return g.sqlite_db
+#
+#
+# def init_db():
+#     """
+#     will go to the schema flile
+#     :return: VOID
+#     """
+#     db = get_db()
+#     with app.open_resource('schema.sql', mode='r') as f:
+#         db.cursor().executescript(f.read())
+#     db.commit()
+#
+#
+# def show_entries(SQL):
+#     """
+#     Example show_entries('select title, text from entries order by id desc')
+#     :param SQL: SQL code that will call table info to be returned
+#     :return: entries form sql table look up
+#     """
+#     db = get_db()
+#     cur = db.execute(SQL)
+#     entries = cur.fetchall()
+#     return entries
--- PyCharmPOAProject/PyCharmPOAProject.py
+++ PyCharmPOAProject/PyCharmPOAProject.py
@@ -2,22 +2,38 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-from DataBase.DataBaseControls.FlaskDatabaseMangment import init_db, get_db,connect_db,show_entries  #this should import the FlaskDatabaseMangment.py file Note you will need to change this if there is ever a change in file stucture
+# from DataBase.DataBaseControls.FlaskDatabaseMangment import init_db,show_entries  #this should import the FlaskDatabaseMangment.py file Note you will need to change this if there is ever a change in file stucture
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path + "//DataBase", 'POA.db'),
+    DATABASE=os.path.join(app.root_path, 'POA.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
-app.config.from_envvar('PyCharmPOAProject_SETTINGS', silent=True)
+app.config.from_envvar('PYCHARMPOAPROJECT_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_entries():
+    print("loading")
+    db = get_db()
+    print("Got DB")
+    cur = db.execute('select Trip_Name, Trip_Capacity, Trip_Info, Trip_Participants from  Trips order by id desc')
+    print("got cur")
+    entries = cur.fetchall()
+    print("all feched")
+    return render_template("POACreateTrip.html", entries=entries)
 
 
 @app.cli.command('initdb')
@@ -36,15 +52,62 @@ def close_db(error):
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    info = [request.form['TripName'], request.form['CarCap'], request.form['TripDescription'], request.form["LeaderName"]]
+    print(info)
+    db.execute('insert into Trips (Trip_Name,Trip_Capacity, Trip_Info, Trip_Participants ) values (?, ?, ?, ?)',info)#Fairly obvious but make sure that the ? marks are the same as the number feilds
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+#*****************************************************************
+#*****************************************************************
+#Database Functions that are here temp will be moved to FlaskDatabase Mangment in the future
+#*****************************************************************
+#*****************************************************************
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# Calls connect DB
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def init_db():
+    """
+    will go to the schema flile
+    :return: VOID
+    """
+    # db = get_db()
+    # with app.open_resource('schema.sql', mode='r') as f:
+    #     db.cursor().executescript(f.read())
+    # db.commit()
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('ApplicationDataManger.sql', mode = 'r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def show_entries(SQL):
+    """
+    Example show_entries('select title, text from entries order by id desc')
+    :param SQL: SQL code that will call table info to be returned
+    :return: entries form sql table look up
+    """
+    db = get_db()
+    cur = db.execute(SQL)
+    entries = cur.fetchall()
+    return entries
 
 if __name__ == '__main__':
     app.run()
+    app.debug = True
