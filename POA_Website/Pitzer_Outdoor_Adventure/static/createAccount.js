c292e8b0059b0fbd0d6d7ebe29b84b6be11d49cc
CS70 owl <mvonallm@cs.hmc.edu>
2018-10-28 12:12:51 -0700

    Allergies and Diet Restriction formatting
    Both are formatted using a drop down menu/buttons and a text box that
    appears only when selecting a certain option for either (i.e. "other").
    Actually submitting the form still does not work.

--- POA_Website/Pitzer_Outdoor_Adventure/static/createAccount.js
+++ POA_Website/Pitzer_Outdoor_Adventure/static/createAccount.js
@@ -1,8 +1,59 @@
-/**
- * Created by matth_000 on 10/7/2018.
- */
-console.log($("#Other_Diet_Restrictions_Box"));
-//$("input[value='Other_Diet_Restrictions_Box']").hide();
-$("#Other_Diet_Restrictions_Box").hide();
-$('[data-toggle="tooltip"]').tooltip();
-console.log("toolTips");
+// /**
+//  * Created by matth_000 on 10/7/2018.
+//  */
+// console.log($("#Other_Diet_Restrictions_Box"));
+// //$("input[value='Other_Diet_Restrictions_Box']").hide();
+// $("#Other_Diet_Restrictions_Box").hide();
+// $('[data-toggle="tooltip"]').tooltip();
+// console.log("toolTips");
+
+$(document).ready(function ()
+{
+    allergiesVisible = false;
+    dietRestrictionsVisible = false;
+    $("#otherAllergies").change(function ()
+    {
+        console.log("change");
+        if (!allergiesVisible)
+        {
+            var alternateField = "<input id=\"Other_Allergies_Box\" name=\"Other_Allergies_Box\" class=\"form-control\" required=\"\" type=\"text\">";
+            $("#otherAllergiesDiv").append(alternateField);
+            allergiesVisible = true;
+        }
+        else
+        {
+            $("#Other_Allergies_Box").remove();
+            allergiesVisible = false;
+        }
+    });
+    $("#Diet_Restrictions_Box").change(function ()
+    {
+        console.log("diet restriction changes");
+        console.log($(this)[0].value);
+        console.log(dietRestrictionsVisible);
+        if ($(this)[0].value === "Other" && !dietRestrictionsVisible)
+        {
+            var alternateField = "<input id=\"Other_Diet_Restrictions_Box\" name=\"Other_Diet_Restrictions_Box\" class=\"form-control\" required=\"\" type=\"text\">";
+            console.log("yay im workin");
+            $("#otherRestrictionsDiv").append(alternateField);
+            dietRestrictionsVisible = true;
+        }
+        else if ($(this)[0].value != "Other" && dietRestrictionsVisible)
+        {
+            console.log("the bads are happening");
+            $("#Other_Diet_Restrictions_Box").remove();
+            dietRestrictionsVisible = false;
+        }
+    });
+    $("#deleteRow").click(function ()
+    {
+        //console.log("yay im workin");
+        if (counter > 0)
+        {
+            $("#costName" + counter).remove();
+            $("#costMagnitude" + counter).remove();
+            $("#para" + counter).remove();
+            counter--;
+        }
+    });
+});
--- POA_Website/Pitzer_Outdoor_Adventure/static/createTripModal.js
+++ POA_Website/Pitzer_Outdoor_Adventure/static/createTripModal.js
@@ -1,26 +1,26 @@
-    $(document).ready(function ()
-    {
-       console.log("loaded code for modal");
-        var counter = 1;
+$(document).ready(function ()
+{
+   console.log("loaded code for modal");
+    var counter = 1;
 
-        $("#addRow").click(function ()
-        {
-            counter++;
-            var newCost = "<input id=\"costName" + counter + "\" name=\"costName" + counter + "\" class=\"form-control\" required=\"\" type=\"text\" placeholder=\"Name of Expense\"> <input id=\"costMagnitude" + counter + "\" name=\"costMagnitude" + counter + "\" class=\"form-control\" required=\"\" type=\"text\" placeholder=\"Cost\" data-parsley-type=\"number\"> <p id=para" + counter + "></p>";
-            console.log("yay im workin");
-            //$("#additionalCostBreakdownDiv").append($(".cost"));
-            $("#additionalCostBreakdownDiv").append(newCost);
-        });
-        $("#deleteRow").click(function ()
-        {
-            //console.log("yay im workin");
-            if (counter > 0)
-            {
-                $("#costName" + counter).remove();
-                $("#costMagnitude" + counter).remove();
-                $("#para" + counter).remove();
-                counter--;
-            }
-        });
+    $("#addRow").click(function ()
+    {
+        counter++;
+        var newCost = "<input id=\"costName" + counter + "\" name=\"costName" + counter + "\" class=\"form-control\" required=\"\" type=\"text\" placeholder=\"Name of Expense\"> <input id=\"costMagnitude" + counter + "\" name=\"costMagnitude" + counter + "\" class=\"form-control\" required=\"\" type=\"text\" placeholder=\"Cost\" data-parsley-type=\"number\"> <p id=para" + counter + "></p>";
+        console.log("yay im workin");
+        //$("#additionalCostBreakdownDiv").append($(".cost"));
+        $("#additionalCostBreakdownDiv").append(newCost);
     });
+    $("#deleteRow").click(function ()
+    {
+        //console.log("yay im workin");
+        if (counter > 0)
+        {
+            $("#costName" + counter).remove();
+            $("#costMagnitude" + counter).remove();
+            $("#para" + counter).remove();
+            counter--;
+        }
+    });
+});
 
